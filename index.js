0f31d29d7c8b6d99d181026e2095a3befbce95af
docshark <docshark@gmail.com>
2014-12-21 13:05:55 +0200

    Node app

---
+++ index.js
@@ -0,0 +1,8 @@
+var express = require("express"),
+  app = express(),
+  port = 8080;
+app.get('*', function(req,res){
+  res.end('Hello world');
+});
+app.listen(port);
+console.log('Server is up on port ' + port);
